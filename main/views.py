c448f08c8c564eff8d50a517cc8df9571a2e2536
wwan13 <wwan13@naver.com>
2020-08-05 20:16:04 +0900

    views.py - crud 구현 .tw

--- main/views.py
+++ main/views.py
@@ -1,5 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from .models import Item
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def creat(request): #Creat - 객체 생성 부분
+    if request.method == 'POST':
+        item = Item()
+        item.name = request.POST['name']
+        item.store = request.POST['store']
+        item.menu = request.POST['menu']
+        item.option = request.POST['option']
+        item.option = request.POST['price']
+
+        item.save()
+        return redirect('home')
+
+def upadte(request): #Update - 객체 수정
+    item = get_object_or_404(Item, pk = item_id)
+    if request.method == 'POST':
+        item = Item()
+        item.name = request.POST['name']
+        item.store = request.POST['store']
+        item.menu = request.POST['menu']
+        item.option = request.POST['option']
+        item.option = request.POST['price']
+
+        item.save()
+        return redirect('home')
+    else:
+        return render(request,'update.html', {'items':item})
+
+def delete(request, item_id): #Delete - 객체 삭제
+    item = get_object_or_404(Item, pk = item_id)
+    item.delete()
+
+    return redirect('home')
