f1ce3b726e981d19b2ce6a38ff732a90bd88c638
wwan13 <wwan13@naver.com>
2020-08-09 17:54:48 +0900

    models.py 수정

--- main/models.py
+++ main/models.py
@@ -14,6 +14,7 @@ class User(models.Model):
     store = models.CharField(max_length = 50, default='')
     menu = models.CharField(max_length = 50, default='')
     option = models.TextField(default='option')
+    price = models.IntegerField()
 
     def __str__(self):
         return self.store + " >> " + self.menu
